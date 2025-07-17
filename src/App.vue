cbb3e8606172c135cddcfd5e3d690907620afcab
unknown <1114ny@naver.com>
2025-07-17 14:41:10 +0900

    section.03-16

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,15 @@
       class="card mt-2"
     >
       <div class="card-body p-2">
-        {{ todo.subject }}
+        <div class="form-check">
+          <input 
+            class="form-check-input"
+            type="checkbox"
+            v-model="todo.completed"
+            >
+          <label
+            class="form-check-label">{{ todo.subject }}</label>
+        </div>
       </div>
     </div>
 </div>
@@ -44,14 +52,8 @@ import { ref } from 'vue';
 
 export default ({
   setup(){
-    const toggle = ref(false);
     const todo = ref('');
-    const todos = ref([
-      {id: 1, subject: '휴대폰 사기'},
-      {id: 2, subject: '장보기'},
-
-    ]);
-
+    const todos = ref([]);
     const hasError = ref(false);
 
     const onSubmit = () => {
@@ -60,24 +62,18 @@ export default ({
       }else{
         todos.value.push({
         id: Date.now(),
-        subject: todo.value
+        subject: todo.value,
+        completed: true,
       });
       hasError.value = false;
-
+      todo.value = '';
       }
-   
-    }
-
-    const onToggle = () => {
-      toggle.value = !toggle.value;
-    }
+    };
 
     return {
       todo,
       todos,
       onSubmit,
-      toggle,
-      onToggle,
       hasError
     };
   }
