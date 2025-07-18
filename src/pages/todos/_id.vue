d69754dedafa0c0fbb781c8be47837ead0fe7d7f
unknown <1114ny@naver.com>
2025-07-18 15:31:23 +0900

    section.03-48

---
+++ src/pages/todos/_id.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Todo Page
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router';
+
+    const route = useRoute();
+
+    console.log(route.params.id)
+
+</script>
+
+<style>
+
+</style>
