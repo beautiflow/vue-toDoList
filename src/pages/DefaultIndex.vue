dc6afb3cd954a8427f3d774580b63f682c4409d6
unknown <1114ny@naver.com>
2025-07-24 15:33:54 +0900

    section.15-77

--- src/pages/DefaultIndex.vue
+++ src/pages/DefaultIndex.vue
@@ -1,9 +1,15 @@
 <script setup>
+import { useCount } from '@/hooks/count';
+
+const { count } = useCount();
+console.log(count);
 
 </script>
 
 <template>
   <div>Home page</div>
+  <div>{{ count }}</div>
+  <button @click="count++">Add</button>
 </template>
 
 <style>
